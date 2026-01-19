a5c3939aacc683ef81ec9adc1e0f32453192b3b7
abkein <rickbatra0z@gmail.com>
2026-01-19 05:42:05 +0300

    Add flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "C++ dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        cmake
+        ninja
+        pkg-config
+        gcc
+        gdb
+
+        (python3.withPackages (ps: with ps; [
+          ipykernel
+          pip
+          bash_kernel
+          ipython
+          ipykernel
+          jupyter
+          jupyterlab
+          notebook
+          pyzmq
+          numpy
+          pandas
+          scipy
+          requests
+          matplotlib
+        ]))
+
+        openmpi
+      ];
+    };
+  };
+}
