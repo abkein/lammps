2be342d842732a7e8d61bd9e9cd8038c7d90da77
abkein <rickbatra0z@gmail.com>
2026-01-20 20:50:11 +0300

    Formatting

--- src/nucc_cspan.hpp
+++ src/nucc_cspan.hpp
@@ -2,11 +2,8 @@
 #define __NUCC_CUSTOM_CSPAN_HPP
 
 #include "memory.h"
-#include "nucc_defs.hpp"
 
 #include <algorithm>
-#include <concepts>
-#include <iostream>
 #include <span>
 #include <type_traits>
 
@@ -31,7 +28,8 @@ T zero_value()
 }
 
 #ifdef __NUCC_CSPAN_DEBUG_CALLS
-#  include <iostream>
+#include <iostream>
+// dumb thing used to catch calls that are inlined by default
 struct Kallbeck {
   __attribute_noinline__ void call() const noexcept
   {
@@ -44,148 +42,148 @@ struct Kallbeck {
 template <typename T, std::size_t Extent = std::dynamic_extent>
 class cspan {
  public:
-  cspan() noexcept = default;
-  cspan(const std::span<T, Extent> &span) = delete;
-  cspan(const cspan<T, Extent> &other) = delete;
-  constexpr cspan(T *ptr, std::size_t n) noexcept : span_(std::span<T, Extent>(ptr, n)) {}
-  constexpr cspan(T *begin, T *end) noexcept : span_(std::span<T, Extent>(begin, end)) {}
-  constexpr cspan(std::span<T, Extent> &&span) noexcept : span_(span) {}
-  constexpr cspan(cspan<T, Extent> &&other) noexcept : span_(std::move(other.span_)) {}
+  cspan() noexcept                        = default;
+  cspan(const std::span<T, Extent>& span) = delete;
+  cspan(const cspan<T, Extent>& other)    = delete;
+  constexpr cspan(T* ptr, std::size_t n) noexcept : span_(std::span<T, Extent>(ptr, n)) {}
+  constexpr cspan(T* begin, T* end) noexcept : span_(std::span<T, Extent>(begin, end)) {}
+  constexpr cspan(std::span<T, Extent>&& span) noexcept : span_(span) {}
+  constexpr cspan(cspan<T, Extent>&& other) noexcept : span_(std::move(other.span_)) {}
   template <typename U, std::size_t OtherExtent>
     requires(std::is_convertible_v<U (*)[], T (*)[]>)
-  constexpr cspan(const cspan<U, OtherExtent> &other) noexcept : span_(other.data(), other.size())
+  constexpr cspan(const cspan<U, OtherExtent>& other) noexcept : span_(other.data(), other.size())
   {
   }
-  inline constexpr cspan<T, Extent> &operator=(std::span<T, Extent> &&span) noexcept
+  inline constexpr cspan<T, Extent>& operator=(std::span<T, Extent>&& span) noexcept
   {
     span_ = span;
     return *this;
   }
-  inline constexpr cspan<T, Extent> &operator=(cspan<T, Extent> &&other) noexcept
+  inline constexpr cspan<T, Extent>& operator=(cspan<T, Extent>&& other) noexcept
   {
     span_ = std::move(other.span_);
     return *this;
   }
 
-  inline constexpr T &at(std::size_t index)
+  inline constexpr T& at(std::size_t index)
 #ifndef __NUCC_CSPAN_CHECK_ACCESS
       noexcept
 #endif
   {
 #ifdef __NUCC_CSPAN_CHECK_ACCESS
     if (index >= span_.size()) {
-#  ifdef __NUCC_CSPAN_DEBUG_CALLS
+#ifdef __NUCC_CSPAN_DEBUG_CALLS
       beck.call();
-#  endif
+#endif
       throw std::out_of_range("Index out of range");
     }
 #endif
     return span_[index];
   }
 
-  inline constexpr T *offset(std::size_t offset)
+  inline constexpr T* offset(std::size_t offset)
 #ifndef __NUCC_CSPAN_CHECK_ACCESS
       noexcept
 #endif
   {
 #ifdef __NUCC_CSPAN_CHECK_ACCESS
     if (offset >= span_.size()) {
-#  ifdef __NUCC_CSPAN_DEBUG_CALLS
+#ifdef __NUCC_CSPAN_DEBUG_CALLS
       beck.call();
-#  endif
+#endif
       throw std::out_of_range("Index out of range");
     }
 #endif
     return span_.data() + offset;
   }
 
-  inline constexpr T &operator[](std::size_t index)
+  inline constexpr T& operator[](std::size_t index)
 #ifndef __NUCC_CSPAN_CHECK_ACCESS
       noexcept
 #endif
   {
 #ifdef __NUCC_CSPAN_CHECK_ACCESS
     if (index >= span_.size()) {
-#  ifdef __NUCC_CSPAN_DEBUG_CALLS
+#ifdef __NUCC_CSPAN_DEBUG_CALLS
       beck.call();
-#  endif
+#endif
       throw std::out_of_range("Index out of range");
     }
 #endif
     return span_[index];
   }
 
-  inline constexpr const T &at(std::size_t index) const
+  inline constexpr const T& at(std::size_t index) const
 #ifndef __NUCC_CSPAN_CHECK_ACCESS
       noexcept
 #endif
   {
 #ifdef __NUCC_CSPAN_CHECK_ACCESS
     if (index >= span_.size()) {
-#  ifdef __NUCC_CSPAN_DEBUG_CALLS
+#ifdef __NUCC_CSPAN_DEBUG_CALLS
       beck.call();
-#  endif
+#endif
       throw std::out_of_range("Index out of range");
     }
 #endif
     return span_[index];
   }
 
-  inline constexpr const T *offset(std::size_t offset) const
+  inline constexpr const T* offset(std::size_t offset) const
 #ifndef __NUCC_CSPAN_CHECK_ACCESS
       noexcept
 #endif
   {
 #ifdef __NUCC_CSPAN_CHECK_ACCESS
     if (offset >= span_.size()) {
-#  ifdef __NUCC_CSPAN_DEBUG_CALLS
+#ifdef __NUCC_CSPAN_DEBUG_CALLS
       beck.call();
-#  endif
+#endif
       throw std::out_of_range("Index out of range");
     }
 #endif
     return span_.data() + offset;
   }
 
-  inline constexpr const T &operator[](std::size_t index) const
+  inline constexpr const T& operator[](std::size_t index) const
 #ifndef __NUCC_CSPAN_CHECK_ACCESS
       noexcept
 #endif
   {
 #ifdef __NUCC_CSPAN_CHECK_ACCESS
     if (index >= span_.size()) {
-#  ifdef __NUCC_CSPAN_DEBUG_CALLS
+#ifdef __NUCC_CSPAN_DEBUG_CALLS
       beck.call();
-#  endif
+#endif
       throw std::out_of_range("Index out of range");
     }
 #endif
     return span_[index];
   }
 
-  inline constexpr T *data() noexcept { return span_.data(); }
+  inline constexpr T* data() noexcept { return span_.data(); }
 
-  inline constexpr const T *data() const noexcept { return span_.data(); }
+  inline constexpr const T* data() const noexcept { return span_.data(); }
 
   inline constexpr std::size_t size() const noexcept { return span_.size(); }
 
-  void destroy(LAMMPS_NS::Memory *memory) noexcept
+  void destroy(LAMMPS_NS::Memory* memory) noexcept
   {
-    T *ptr = span_.data();
+    T* ptr = span_.data();
     memory->destroy(ptr);
     span_ = std::span<T, Extent>();
   }
 
-  void create(LAMMPS_NS::Memory *memory, std::size_t n, const char *name)
+  void create(LAMMPS_NS::Memory* memory, std::size_t n, const char* name)
   {
-    T *ptr;
+    T* ptr;
     memory->create(ptr, n, name);
     span_ = std::span<T, Extent>(ptr, n);
   }
 
-  void grow(LAMMPS_NS::Memory *memory, std::size_t n, const char *name)
+  void grow(LAMMPS_NS::Memory* memory, std::size_t n, const char* name)
   {
-    T *ptr = span_.data();
+    T* ptr = span_.data();
     memory->grow(ptr, n, name);
     span_ = std::span<T, Extent>(ptr, n);
   }
@@ -198,7 +196,7 @@ class cspan {
     if (!span_.empty()) { std::fill_n(span_.data(), span_.size(), zero_value<T>()); }
   }
 
-  template<typename U>
+  template <typename U>
   inline void reset_unsafe(const U& value)
     requires(!std::is_const_v<T>)
   {
@@ -206,15 +204,18 @@ class cspan {
     if (!span_.empty()) { std::fill(reinterpret_cast<U*>(span_.data()), reinterpret_cast<U*>(span_.data() + span_.size()), value); }
   }
 
-  template<typename U>
+  template <typename U>
   inline void reset_unsafe(U&& value)
     requires(!std::is_const_v<T>)
   {
     static_assert(sizeof(T) % sizeof(U) == 0, "Object size is not multiple of target value size");
-    if (!span_.empty()) { std::fill(reinterpret_cast<std::remove_const_t<U>*>(span_.data()), reinterpret_cast<std::remove_const_t<U>*>(span_.data() + span_.size()), value); }
+    if (!span_.empty()) {
+      std::fill(reinterpret_cast<std::remove_const_t<U>*>(span_.data()), reinterpret_cast<std::remove_const_t<U>*>(span_.data() + span_.size()),
+                value);
+    }
   }
 
-  inline constexpr std::size_t memory_usage() const noexcept { return size() * sizeof(T) + sizeof(std::size_t) + sizeof(T *); }
+  inline constexpr std::size_t memory_usage() const noexcept { return size() * sizeof(T) + sizeof(std::size_t) + sizeof(T*); }
 
  private:
   std::span<T, Extent> span_;
