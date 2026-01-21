b7c1c6d61c1ad20587c8b5b32bbc25278d466306
abkein <rickbatra0z@gmail.com>
2026-01-21 20:05:01 +0300

    Formatting, const

--- src/compute_cluster_size_ext.h
+++ src/compute_cluster_size_ext.h
@@ -19,35 +19,35 @@ ComputeStyle(size/cluster/ext,ComputeClusterSizeExt);
 // clang-format on
 #else
 
-#  ifndef LMP_COMPUTE_CLUSTER_SIZE_ExT_H
-#    define LMP_COMPUTE_CLUSTER_SIZE_ExT_H
+#ifndef LMP_COMPUTE_CLUSTER_SIZE_ExT_H
+#define LMP_COMPUTE_CLUSTER_SIZE_ExT_H
 
-#    include "compute.h"
-#    include "nucc_allocator.hpp"
-#    include "nucc_defs.hpp"
-#    include "nucc_cluster_data.hpp"
-#    include "nucc_cspan.hpp"
+#include "compute.h"
+#include "nucc_allocator.hpp"
+#include "nucc_cluster_data.hpp"
+#include "nucc_cspan.hpp"
+#include "nucc_defs.hpp"
 
-#    include <array>
-#    include <scoped_allocator>
-#    include <span>
-#    include <unordered_map>
-#    include <vector>
+#include <array>
+#include <scoped_allocator>
+#include <span>
+#include <unordered_map>
+#include <vector>
 
 namespace NUCC {
-  struct cldata {
-    cldata() = default;
-    cldata(int id, int sz): id(id), sz(sz) {}
-    int id = 0;
-    int sz = 0;
-  };
-}
+struct cldata {
+  cldata() = default;
+  cldata(int id, int sz) : id(id), sz(sz) {}
+  int id = 0;
+  int sz = 0;
+};
+}    // namespace NUCC
 
 namespace LAMMPS_NS {
 
 class ComputeClusterSizeExt : public Compute {
  public:
-  ComputeClusterSizeExt(class LAMMPS *lmp, int narg, char **arg);
+  ComputeClusterSizeExt(class LAMMPS* lmp, int narg, char** arg);
   ~ComputeClusterSizeExt() noexcept(true) override;
   void init() override;
   void compute_vector() override;
@@ -55,57 +55,61 @@ class ComputeClusterSizeExt : public Compute {
   double memory_usage() override;
 
   inline constexpr int get_size_cutoff() const noexcept(true) { return size_cutoff; }
-  inline constexpr NUCC::cspan<const double> get_data() const noexcept { return dist; }
+  inline constexpr const NUCC::cspan<const double> get_data() const noexcept { return NUCC::cspan<const double>(dist); }
   inline constexpr int get_nonexclusive() const noexcept(true) { return nonexclusive; }
-  inline constexpr const std::unordered_map<int, int> &get_cluster_map() const noexcept(true) { return cluster_map; }
-  inline constexpr const std::unordered_map<int, std::vector<int>> &get_cIDs_by_size() const noexcept(true) { return cIDs_by_size; }
-  inline constexpr const std::unordered_map<int, std::vector<int>> &get_cIDs_by_size_all() const noexcept(true) { return cIDs_by_size_all; }
-//   inline constexpr const NUCC::Map_t<int, int> *get_cluster_map() const noexcept(true) { return cluster_map; }
-//   inline constexpr const NUCC::Map_t<int, NUCC::Vec_t<int>> *get_cIDs_by_size_my() const noexcept { return cIDs_by_size; }
-//   inline constexpr const NUCC::Map_t<int, NUCC::Vec_t<int>> *get_cIDs_by_size() const noexcept { return cIDs_by_size_all; }
-  inline constexpr const NUCC::cspan<const NUCC::cluster_data> get_clusters() const noexcept(true) { return clusters; }
+  inline constexpr const std::unordered_map<int, int>& get_cluster_map() const noexcept(true) { return cluster_map; }
+  inline constexpr const std::unordered_map<int, std::vector<int>>& get_clid_by_size() const noexcept(true) { return clid_by_size; }
+  inline constexpr const std::unordered_map<int, std::vector<int>>& get_clid_by_size_all() const noexcept(true) { return clid_by_size_global; }
+  //   inline constexpr const NUCC::Map_t<int, int> *get_cluster_map() const noexcept(true) { return cluster_map; }
+  //   inline constexpr const NUCC::Map_t<int, NUCC::Vec_t<int>> *get_cIDs_by_size_my() const noexcept { return cIDs_by_size; }
+  //   inline constexpr const NUCC::Map_t<int, NUCC::Vec_t<int>> *get_cIDs_by_size() const noexcept { return cIDs_by_size_all; }
+  inline constexpr const NUCC::cspan<const NUCC::cluster_data> get_clusters() const noexcept(true)
+  {
+    return NUCC::cspan<const NUCC::cluster_data>(clusters);
+  }
 
  private:
-  int size_cutoff;    // number of elements reserved in dist
+  int size_cutoff;    // number of elements reserved in resulting distribution, i.e. the max size of cluster distribution will be built to
 
-//   NUCC::MemoryKeeper *keeper1;
-//   NUCC::MapAlloc_t<int, int> *cluster_map_allocator;
-//   NUCC::Map_t<int, int> *cluster_map;
-  std::unordered_map<int, int> cluster_map;    // clid -> idx
+  //   NUCC::MemoryKeeper *keeper1;
+  //   NUCC::MapAlloc_t<int, int> *cluster_map_allocator;
+  //   NUCC::Map_t<int, int> *cluster_map;
+  std::unordered_map<int, int> cluster_map;    // mapping from cluster id to its local indices (idx) in `clusters` and `ns`
 
-//   NUCC::MemoryKeeper *keeper2;
-//   NUCC::MapAlloc_t<int, NUCC::Vec_t<int>> *alloc_map_vec1;
-//   NUCC::Map_t<int, NUCC::Vec_t<int>> *cIDs_by_size;
-  std::unordered_map<int, std::vector<int>> cIDs_by_size;    // size -> vector(idx)
+  //   NUCC::MemoryKeeper *keeper2;
+  //   NUCC::MapAlloc_t<int, NUCC::Vec_t<int>> *alloc_map_vec1;
+  //   NUCC::Map_t<int, NUCC::Vec_t<int>> *cIDs_by_size;
+  std::unordered_map<int, std::vector<int>> clid_by_size;    // mapping from cluster size to vector of local indices (vector(idx))
 
-//   NUCC::MemoryKeeper *keeper3;
-//   NUCC::MapAlloc_t<int, NUCC::Vec_t<int>> *alloc_map_vec2;
-//   NUCC::Map_t<int, NUCC::Vec_t<int>> *cIDs_by_size_all;
-  std::unordered_map<int, std::vector<int>> cIDs_by_size_all;
+  //   NUCC::MemoryKeeper *keeper3;
+  //   NUCC::MapAlloc_t<int, NUCC::Vec_t<int>> *alloc_map_vec2;
+  //   NUCC::Map_t<int, NUCC::Vec_t<int>> *cIDs_by_size_all;
+  std::unordered_map<int, std::vector<int>> clid_by_size_global;    // don't know if it's usable at all, maybe delete
 
-  int nloc = 0;                      // number of reserved elements in atoms_by_cID and cIDs_by_size
-  NUCC::cspan<double> dist;          // cluster size distribution (vector == dist)
-  NUCC::cspan<double> dist_local;    // local cluster size distribution
-  int nc_global = 0;                 // number of clusters total
-  NUCC::cspan<int> counts_global;
-  NUCC::cspan<int> displs;
-  NUCC::cspan<NUCC::cluster_data> clusters;
-  NUCC::cspan<NUCC::cldata> ns;
+  int nloc = 0;                                // possible count of clusters (nlocal*damp), size of allocated arrays
+  NUCC::cspan<double> dist;                    // cluster size distribution (vector == dist)
+  NUCC::cspan<double> dist_local;              // local cluster size distribution
+  int nc_global = 0;                           // actual number of clusters (across all procs)
+  NUCC::cspan<int> counts_global;              // counts for MPI communication
+  NUCC::cspan<int> displs;                     // displacements for MPI communication
+  NUCC::cspan<NUCC::cluster_data> clusters;    // cluster data
+  NUCC::cspan<NUCC::cldata>
+      ns;    // array of cluster ids and their sizes. same as `clusters`, but containing reduced data; used to communicate with other procs
   NUCC::cspan<NUCC::cldata> gathered;
-  NUCC::cspan<double> peratom_size;
-  bigint natom_loc = 0;
-  int nonexclusive = 0;
-  int nloc_peratom = 0;
+  NUCC::cspan<double> peratom_size;    // size of cluster this atom belongs to
+  bigint nloc_gather = 0;
+  int nonexclusive   = 0;    // number of clusters not owned by this proc
+  int nloc_peratom   = 0;    // number of elements allocated for peratom array
 
-  NUCC::cspan<int> monomers;
-  int nmono = 0;
+  NUCC::cspan<int> monomers;            // local indices of monomers
+  int nmono                     = 0;    // number of monomers
 
-  Compute *compute_cluster_atom = nullptr;
+  Compute* compute_cluster_atom = nullptr;
 
   // MPI_Datatype MPI_CLDATA;
 };
 
 }    // namespace LAMMPS_NS
 
-#  endif
+#endif
 #endif
