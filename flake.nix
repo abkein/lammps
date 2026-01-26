2355f8c0eb9ffec3d5814d3ab11e99ea54ce99f6
abkein <rickbatra0z@gmail.com>
2026-01-26 14:11:12 +0300

    YAFU

--- flake.nix
+++ flake.nix
@@ -7,40 +7,64 @@
     { self, nixpkgs }:
 
     let
+      root = "/home/kein/repos/mylammps";
+
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
       lib = pkgs.lib;
+
       gccToolchain = pkgs.gcc.cc; # "real" gcc, not just the wrapper
       mpi = pkgs.openmpi;
-      clangTidyWrapped = pkgs.writeShellScriptBin "clang-tidy" ''
-        exec ${lib.getExe' pkgs.clang "clang-tidy"} \
+      llvm = pkgs.llvmPackages_latest;
+
+      clangTidy = lib.getExe' llvm.clang-tools "clang-tidy";  # lib.getExe' pkgs.clang "clang-tidy"
+      clangTidyWrapped = pkgs.writeShellScriptBin "clang-tidy-wrapped" ''
+        exec ${clangTidy} \
           --extra-arg=--gcc-toolchain=${gccToolchain} \
           --extra-arg=-stdlib=libstdc++ \
           --extra-arg=-isystem${mpi}/include \
+          --extra-arg=-Qunused-arguments \
           "$@"
       '';
       workspaceFile = pkgs.writeText "LAMMPS.code-workspace" (
         builtins.toJSON {
           folders = [
             {
-              path = "/home/kein/repos/mylammps";
+              path = root;
             }
           ];
           settings = {
-            "direnv.watchForChanges" = false;
-            "cmake.sourceDirectory" = "/home/kein/repos/mylammps/cmake";
+            # "direnv.watchForChanges" = false;
+            "cmake.sourceDirectory" = "${root}/cmake";
 
             "cpplint.cpplintPath" = lib.getExe' pkgs.cpplint "cpplint";
             "cpplint.lineLength" = 300;
             "cpplint.verbose" = 0;
+            # "cpplint.filters" =
+            #   "build/include_subdir,readability/todo,whitespace/newline,whitespace/operators,whitespace/braces,build/include_order";
 
-            "c-cpp-linter.clangTidy.path" = clangTidyWrapped;
+            "c-cpp-linter.clangTidy.enabled" = false;
+            "c-cpp-linter.clangTidy.path" = "${clangTidyWrapped}/bin/clang-tidy-wrapped";
             "c-cpp-linter.compiler.path" = lib.getExe' pkgs.clang "clang++";
+            "c-cpp-linter.compiler.additionalFlags" = [
+              "-Qunused-arguments"
+            ];
             "c-cpp-linter.cppCheck.path" = lib.getExe' pkgs.cppcheck "cppcheck";
-            "c-cpp-linter.general.runOnOpen" = false;
-            "c-cpp-linter.general.runOnSave" = false;
-            "c-cpp-linter.general.showInformationDialog" = true;
-            "c-cpp-linter.general.showOutputFromLinters" = true;
+            "c-cpp-linter.cppCheck.additionalFlags" = [
+              "--std=c++20"
+              "--platform=native"
+              "--check-level=exhaustive"
+              "--force"
+            ];
+
+            "clang-tidy.buildPath" = "${root}/build";
+            "clang-tidy.lintOnSave" = false;
+            "clang-tidy.executable" = "${clangTidyWrapped}/bin/clang-tidy-wrapped";
+            "clang-tidy.checks" = [
+              "-*,boost-*,bugprone-*,concurrency-*,hicpp-*,modernize-*,performance-*,readability-*,llvm-*,misc-*,mpi-*,openmp-*"
+              "-readability-magic-numbers,-readability-function-cognitive-complexity,-readability-identifier-length,-readability-math-missing-parentheses"
+              "-modernize-use-trailing-return-type,-hicpp-signed-bitwise"
+            ];
           };
         }
       );
@@ -56,8 +80,10 @@
           gcc
           gdb
           gfortran
-          clang
-          # clangTidyWrapped
+          # clang
+          llvm.clang
+          llvm.clang-tools
+          clangTidyWrapped
 
           (python3.withPackages (
             ps: with ps; [
